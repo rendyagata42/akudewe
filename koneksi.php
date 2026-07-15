888e6966b43becbaf6e0f5b0cc013b0f90e25cd4
rendyagata42 <rendyagata42@gmail.com>
2026-07-16 03:08:19 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,16 @@
 <?php
 $host = "mysql.railway.internal";
 $user = "root"; 
-$pass = "lomArDSmZldAgioqrGvzPbJdIdGVIFrg";     
+$pass = "lomArDSmZldAgioqrGvzPbJdIdGVIFrg";       
 $db   = "railway";
+$port = 3306; // Port default MySQL di Railway
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("Koneksi ke database gagal: " . $conn->connect_error);
+}
+?>
 
 $conn = new mysqli($host, $user, $pass, $db);
 
